aed6aa0249452727e6d3bd3e0ebfeb93e6e57cf4
Jose Neto <joseneto180201@gmail.com>
2022-08-04 23:14:34 +0000

    thubnail adicionada corretamente

--- src/app.js
+++ src/app.js
@@ -20,10 +20,11 @@ try {
             const profilePicUrl = member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 });
             const message = await new GenRandomMessage(username).getRandomMessage();
 
-            const embed = new MessageEmbed({thumbnail: {url: profilePicUrl}})
+            const embed = new MessageEmbed()
             .setTitle(':partying_face: Uhull!! :partying_face:')
             .setColor("#6a0bd9")
             .setFields({name: "Um presente...", value: "\n\n\nInsira o cupom abaixo na [nossa loja](https://shopee.com.br/minhastack?categoryId=100644&itemId=16403580069) e receba 5% OFF em qualquer produto :partying_face: \n\nMINH5OFF})"})
+            .setThumbnail(profilePicUrl)
             .setDescription(message);
             
             const parseEmbed = embed.toJSON();
