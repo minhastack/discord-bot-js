a7e67cda5c85d4df5458c0d0e3d160a93960a9fd
Jose Neto <joseneto180201@gmail.com>
2022-08-04 22:51:34 +0000

    embed message atualizada

--- src/app.js
+++ src/app.js
@@ -20,14 +20,10 @@ try {
             const profilePicUrl = member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 });
             const message = await new GenRandomMessage(username).getRandomMessage();
 
-            const embed = new MessageEmbed({
-                title: ':partying_face: Uhull!! :partying_face:',
-                description: message,
-                thumbnail: { url: profilePicUrl },
-                color: "#6a0bd9",
-                description: \n\n\n'Insira o cupom abaixo na [nossa loja](https://shopee.com.br/minhastack?categoryId=100644&itemId=16403580069) e receba 5% OFF em qualquer produto :partying_face: \n\nMINH5OFF'
-                
-            });
+            const embed = new MessageEmbed({thumbnail: {url: profilePicUrl}})
+            .setTitle(':partying_face: Uhull!! :partying_face:')
+            .setColor("#6a0bd9")
+            .setFields({name: "Um presente...", value: "\n\n\nInsira o cupom abaixo na [nossa loja](https://shopee.com.br/minhastack?categoryId=100644&itemId=16403580069) e receba 5% OFF em qualquer produto :partying_face: \n\nMINH5OFF})"})
 
             const parseEmbed = embed.toJSON();
             channel.send({embeds: [parseEmbed]});
